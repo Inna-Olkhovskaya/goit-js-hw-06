328cefc913766bc1c966a2101c839b6b9642df46
Inna-Olkhovskaya <115504107+Inna-Olkhovskaya@users.noreply.github.com>
2023-03-02 21:10:17 +0200

    finished task #2

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,17 @@
-/*Напиши скрипт который:
-1. Посчитает и выведет в консоль количество категорий
- в ul#categories, то есть элементов li.item.
-2. Для каждого элемента li.item в списке ul#categories, 
-найдет и выведет в консоль текст заголовка элемента (тега <h2>)
- и количество элементов в категории (всех вложенных в него <li>).
-Для выполнения этой задачи нужно использовать метод forEach() и свойства навигации по DOM.*/
+//Напиши скрипт который:
+
+/* 1. Посчитает и выведет в консоль количество категорий
+ в ul#categories, то есть элементов li.item.*/
 
 const listEl = document.querySelector("#categories");
 const itemEl = listEl.children;
 
 console.log(`Number of categories: ${itemEl.length}`);
+
+/*2. Для каждого элемента li.item в списке ul#categories, 
+найдет и выведет в консоль текст заголовка элемента (тега <h2>)
+ и количество элементов в категории (всех вложенных в него <li>).
+Для выполнения этой задачи нужно использовать метод forEach() и свойства навигации по DOM.*/
 
 const ul = document.querySelectorAll("#categories > li");
 ul.forEach((el) => {
@@ -17,5 +19,3 @@ ul.forEach((el) => {
     `Category: ${el.firstElementChild.textContent}\nElements: ${el.lastElementChild.children.length}`
   );
 });
-
-
--- js/task-02.js
+++ js/task-02.js
@@ -1,15 +1,31 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
-/*Напиши скрипт, который для каждого элемента массива ingredients:
+//Напиши скрипт, который для каждого элемента массива ingredients:
 
-1. Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
-2. Добавит название ингредиента как его текстовое содержимое.
-3. Добавит элементу класс item.
-4. После чего вставит все <li> за одну операцию в список ul#ingredients.*/
+/* 1. Создаст отдельный элемент <>.
+Обзательно используй метод document.createElement().*/
+//2. Добавит название ингредиента как его текстовое содержимое.
+//3. Добавит элементу класс item.
+
+const list = document.querySelector("#ingredients");
+const liArray = [];
+
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.className = "item";
+  item.textContent = ingredient;
+  liArray.push(item);
+});
+
+//4. После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+list.append(...liArray);
+
+
